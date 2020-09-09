fcc5657271ce6cadbf90fec778596d4544a87417
Alex Sherman <asherman.ca@gmail.com>
2020-09-08 21:58:08 -0700

    directory

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 // destcructure title off props
-const MenuItem = ({title}) => (
-  <div className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div style={{
+    backgroundImage: `url(${imageUrl})`
+  }} className={`${size} menu-item`}>
     <div className='content'>
       <div className='title'>{title}</div>
       <span className='subtitle'>SHOP NOW</span>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,25 +1,11 @@
 import React from 'react'
 
 import './homepage.styles.scss';
-import MenuItem from '../../components/menu-item/menu-item.component';
+import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => (
   <div className='homepage'>
-    <h1>Welcome to my Homepage</h1>
-    <div className='directory-menu'>
-      <MenuItem
-        title='HATS'
-      />
-      <MenuItem
-        title='SHOES'
-      />
-      <MenuItem
-        title='JACKETS'
-      />
-      <MenuItem
-        title='SHIRTS'
-      />
-    </div>
+    <Directory/>
   </div>
 );
 
