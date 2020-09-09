283a9b63e4054d611da0ba3ee609a95b7d103fc2
Alex Sherman <asherman.ca@gmail.com>
2020-09-09 10:34:16 -0700

    menu item image

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,11 +4,12 @@ import './menu-item.styles.scss';
 
 // destcructure title off props
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div style={{
-    backgroundImage: `url(${imageUrl})`
-  }} className={`${size} menu-item`}>
+  <div className={`${size} menu-item`}>
+    <div className="background-image" 
+      style={{backgroundImage: `url(${imageUrl})`}}
+    />
     <div className='content'>
-      <div className='title'>{title}</div>
+      <div className='title'>{title.toUpperCase()}</div>
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
