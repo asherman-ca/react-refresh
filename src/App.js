d5b32fa3583006031d7dd90f91949feee5e39e58
Alex Sherman <asherman.ca@gmail.com>
2020-09-11 14:58:16 -0700

    polish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 class App extends React.Component {
-  constructor(){
-    super()
+  // constructor(){
+  //   super()
 
-    this.state = {
-      currentUser: null
-    }
-  }
+  //   this.state = {
+  //     currentUser: null
+  //   }
+  // }
 
   unsubscribeFromAuth = null
 
   componentDidMount(){
+    const { setCurrentUser } = this.props;
     // this connection (onAuthStateChanged) stays open as long as the application component is mounted. Always listening/reactive despite being in didMount
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       // userAuth is null if no one is signed in
@@ -27,16 +30,20 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-            // setstate is async and requires console log to be called in this manner to avoid sync problems
-          }, () => console.log(this.state))
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          })
+          // this.setState({
+          //   currentUser: {
+          //     id: snapShot.id,
+          //     ...snapShot.data()
+          //   }
+          //   // setstate is async and requires console log to be called in this manner to avoid sync problems
+          // }, () => console.log(this.state))
         })
       } else {
-        this.setState({ currentUser: userAuth })
+        setCurrentUser(userAuth)
       }
     })
   }
@@ -48,9 +55,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header
-          currentUser={this.state.currentUser}
-        />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -61,4 +66,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const bindActionToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, bindActionToProps)(App);
