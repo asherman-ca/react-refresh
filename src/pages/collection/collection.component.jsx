72f10afe019fcd906a3aab315a71c7fd2840dbbf
Alex Sherman <asherman.ca@gmail.com>
2020-09-16 15:17:05 -0700

    collection selector and routing

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import './collection.styles.scss'
+import { connect } from 'react-redux'
+import { selectCollection } from '../../redux/shop/shop.selectors'
 
+import './collection.styles.scss'
 import CollectionItem from '../../components/collection-item/collection-item.component'
 
-const CollectionPage = ({ match }) => {
+const CollectionPage = ({ match, collection }) => {
 console.log(match, 'matchCat')
+console.log(collection)
 return (
   <div className="category">
     <h2>CATEGORY PAGE</h2>
@@ -12,4 +15,8 @@ return (
 )
 }
 
-export default CollectionPage
+const bindStoreToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(bindStoreToProps)(CollectionPage)
